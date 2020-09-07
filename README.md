d6e545b027147b6279785c5aeff0380c4dd26724
Brad <bradleofabian@gmail.com>
2020-09-07 10:32:44 -0400

    added methods to readme

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # snamekgame
-A snake game built on js css and html
+
+A snake game built on js css and html. To begin click on the start button. Use your arrow keys to move the snake. Random Apples will appear on the board if you grab an apple the snake will grow and move faster. Hitting the walls of the board will end the game. Click on the restart button to play again.
+
+### JavaScript Methods Used In This Game
+
+querySelector() = returns the first Element within the document that matches the specified selector, or group of selectors.
+
+getElementById() = returns an Element object representing the element whose id property matches the specified string.
+
+createElement() = method creates the HTML element specified by tagName, or an HTMLUnknownElement if tagName isn't recognized.
+
+appendChild() = method adds a node to the end of the list of children of a specified parent node.
+
+addEventListener() = sets up a function that will be called whenever the specified event is delivered to the target
+
+classList.contains() = This will check to see if a certain class is contained on a node element.
+classList.add() = This will add a class to an node element.
+classList.remove() = The will remove a class on a node element.
+
+textContent = This will had a textContent to a Node
+
+setInterval() = calls a function or evaluates an expression at specified intervals (in milliseconds)
+removeInterval() = setInterval will continue until this is called.
+
+unshift() = adds one or more elements to the beginning of an array and returns the new length of the array.
+
+pop() = method removes the last element from an array and returns that element. This method changes the length of the array.
+
+forEach() = method executes a provided function once for each array element.
+
+### CSS Styling To Make This Pop
--- index.js
+++ index.js
@@ -1,44 +1,119 @@
 const grid = document.querySelector(".grid");
-
-const startButton = document.querySelector("#start");
-
-const score = document.querySelector("#score");
-
-let squareArray = [];
+const startButton = document.getElementById("start");
+const scoreDisplay = document.getElementById("score");
+let squares = [];
 let currentSnake = [2, 1, 0];
 let direction = 1;
+const width = 10;
+let appleIndex = 0;
+let intervalTime = 1000;
+let score = 0;
+let speed = 0.5;
+let timerId = 0;
 
 function createGrid() {
-  // create 100 of these elements
-  for (let i = 0; i < 100; i++) {
-    // create elements
+  //create 100 of these elements with a for loop
+  for (let i = 0; i < width * width; i++) {
+    //create element
     const square = document.createElement("div");
-    // add styling to these elements
+    //add styling to the element
     square.classList.add("square");
     //put the element into our grid
     grid.appendChild(square);
-
-    // create array of squares
-    squareArray.push(square);
+    //push it into a new squares array
+    squares.push(square);
   }
-  console.log(squareArray);
 }
-
 createGrid();
-console.log(currentSnake);
-currentSnake.forEach((index) => squareArray[index].classList.add("snake"));
+
+currentSnake.forEach((index) => squares[index].classList.add("snake"));
+
+function startGame() {
+  currentSnake.forEach((index) => squares[index].classList.remove("snake"));
+  //remove the apple
+  squares[appleIndex].classList.remove("apple");
+  clearInterval(timerId);
+  currentSnake = [2, 1, 0];
+  score = 0;
+  //re add new score to browser
+  scoreDisplay.textContent = score;
+  direction = 1;
+  intervalTime = 1000;
+  speed = 0.5;
+  generateApples();
+  currentSnake.forEach((index) => squares[index].classList.add("snake"));
+  timerId = setInterval(move, intervalTime);
+}
 
 function move() {
-  // remove last element from currentSnake array
+  if (
+    (currentSnake[0] + width >= width * width && direction === width) || //if snake has hit bottom
+    (currentSnake[0] % width === width - 1 && direction === 1) || //if snake has hit right wall
+    (currentSnake[0] % width === 0 && direction === -1) || //if snake has hit left wall
+    (currentSnake[0] - width < 0 && direction === -width) || //if snake has hit top
+    squares[currentSnake[0] + direction].classList.contains("snake")
+  )
+    return clearInterval(timerId);
+  //remove last element from our currentSnake array
   const tail = currentSnake.pop();
-  console.log(tail);
-  console.log(currentSnake);
-  // remove styling fro element
-  squareArray[tail].classList.remove("snake");
-  // add square in direction we are heading
-  const head = currentSnake.unshift(currentSnake[0] + direction);
-  // add styling for element
-  squareArray[head].classList.add("snake");
+  //remove styling from last element
+  squares[tail].classList.remove("snake");
+  //add square in direction we are heading
+  currentSnake.unshift(currentSnake[0] + direction);
+
+  // deal with snake head getting apple
+  if (squares[currentSnake[0]].classList.contains("apple")) {
+    //remove the class apple
+    squares[currentSnake[0]].classList.remove("apple");
+    // grow our snake by adding class of snake to it
+    squares[tail].classList.add("snake");
+    // grow our snake array
+    currentSnake.push(tail);
+    // generate a new apple
+    generateApples();
+    // add one to the score
+    score++;
+
+    // display score
+    scoreDisplay.textContent = score;
+    // speed up our snake
+    clearInterval(timerId);
+    intervalTime = intervalTime * speed;
+    timerId = setInterval(move, intervalTime);
+  }
+  //add styling so we can see it
+  squares[currentSnake[0]].classList.add("snake");
 }
 
-move();
+//let timerId = setInterval(move, 1000);
+
+function generateApples() {
+  do {
+    appleIndex = Math.floor(Math.random() * squares.length); //generate a random number
+  } while (squares[appleIndex].classList.contains("snake"));
+  squares[appleIndex].classList.add("apple");
+}
+
+generateApples();
+// 39 is right arrow
+// 38 is for the up arrow
+// 37 is for the left arrow
+// 40 is for the down arrow
+
+function control(e) {
+  if (e.keyCode === 39) {
+    console.log("right pressed");
+    direction = 1;
+  } else if (e.keyCode === 38) {
+    console.log("up pressed");
+    direction = -width;
+  } else if (e.keyCode === 37) {
+    console.log("left pressed");
+    direction = -1;
+  } else if (e.keyCode === 40) {
+    console.log("down pressed");
+    direction = +width;
+  }
+}
+document.addEventListener("keyup", control);
+startButton.addEventListener("click", startGame);
